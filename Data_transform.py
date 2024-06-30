59d1c1872f79f31a71ba9abeb44fc96abb9a896a
Edwin Ibague <edwinibague@usantotomas.edu.co>
2024-06-29 23:49:42 -0500

    optimizacion de bucles

--- Data_transform.py
+++ Data_transform.py
@@ -8,38 +8,32 @@ import numpy as np
 def clean_rows(df, date_index):
     
     df = df.drop(date_index, axis=0)
-    to_drop = []
-    for i in df.interrows():
-        if not df[i[0], "cases"] >= 0 or not df[i[0], "deaths"] >= 0:
-            to_drop.append(i[0])
     
-    if to_drop:
-        df = df.drop(to_drop, axis=0)
+    mask = (df["cases"] >= 0) & (df["deaths"] >= 0)
+    df =df[mask]
     
     df = df.dropna(axis=0, how= 'any')
     
     return df
 
 @task("Validacion de fecha")
+def validate_date(Date):
+    try:
+        perse_date = parser.parse(Date, format="%Y-%m-%d")
+        return perse_date.strftime("%Y-%m-%d")
+    except ValueError:
+        return None
+
 def Date_validation(df):
     
-    index_invalid_format = []
-    flag_cor = False
-    for index, row in df.iterrows():
-        date = row["date"]
-        try:
-            parse_date = parser.parse(date, format="%Y-%m-%d")
-            df.loc[index,"date"] = parse_date.strftime("%Y-%m-%d")
-            flag_cor = True
-            
-        except ValueError:
-            print("Error: correccion de formato de fecha no realizado '{date}', fila: {index+1}")
-            index_invalid_format.append(index)
-        
-    if not flag_cor:
-        print("No han sido necesarias correcciones de formato de fecha en el DataFrame")
+    df["date"] = df["date"].apply(validate_date)
+    invalid_index = df[df["date"].isnull()].index.tolist()
+    if not invalid_index:
+        print("No se requieren correcciones de formato de fecha en el DataFrame")
+    else:
+        print("correcciones realizadas en  las filas: {invalid_index}")
     
-    return df, index_invalid_format
+    return df, invalid_index
 
 
 @task("calculo de nuevos casos")
